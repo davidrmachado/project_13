1734f5b7979ef2a0b2930547bbcb9734b691104d
Matheus Santos <matheus.p.1996@hotmail.com>
2022-08-19 16:05:58 -0300

    Requisito 2 - 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>Login</h1>
+    <main>
+      <h1>Login</h1>
+      <label htmlFor="email-input">
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="email"
+        />
+
+      </label>
+      <label htmlFor="password-input">
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="password"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </main>
   );
 }
 
