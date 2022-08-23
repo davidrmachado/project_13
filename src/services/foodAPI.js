4667e5e4fbbd63b14d5048883100f58d27989ff1
Matheus Santos <matheus.p.1996@hotmail.com>
2022-08-23 15:04:58 -0300

    Requisito 12 (testes incompletos 2)

--- src/services/foodAPI.js
+++ src/services/foodAPI.js
@@ -12,27 +12,6 @@ export async function fetchNationalities() {
   return data;
 }
 
-export async function fetchIngredients(ingrediente) {
-  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-export async function fetchNames(name) {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-export async function fetchFirstLetter(FirstLetter) {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${FirstLetter}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
 export async function fetchImages(ingrediente) {
   const url = `https://www.themealdb.com/images/ingredients/${ingrediente}-Small.png`;
   const response = await fetch(url);
@@ -42,6 +21,34 @@ export async function fetchImages(ingrediente) {
 
 export async function fetchMeals() {
   const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+// ------ requisito 11 ------
+// ------ realiza pesquisa pelo nome do igrediente ------
+
+export async function fetchIngredients(ingrediente) {
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+// ------ realiza pesquisa pelo nome da receita ------
+
+export async function fetchNames(name) {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+// ------ realiza pesquisa pela primeira letra do nome da receita ------
+
+export async function fetchFirstLetter(FirstLetter) {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${FirstLetter}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
