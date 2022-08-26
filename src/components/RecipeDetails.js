591ab60f77de1b906d99edf69745dc5de76fd055
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-08-26 12:54:28 -0300

    arruma para passar no cypress, falta redicionar para favorite e start

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { foodDetailAPI } from '../services/foodAPI';
 import AppContext from '../context/AppContext';
 import { drinkDetailAPI } from '../services/drinkAPI';
 import DetailCards from './DetailCard';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function RecipeDetails({ type, id }) {
+  const history = useHistory();
   const {
     detail,
     setDetail,
@@ -42,18 +44,13 @@ function RecipeDetails({ type, id }) {
 
     return (
       arrayToMap.map((string, index) => (
-        <li
-          key={ index }
-          data-testid={ `${index}-ingredient-name-and-measure` }
-        >
+        <li key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
           {string}
         </li>)));
   };
 
   const handleYoutube = (url) => {
-    const newUrl = url.includes('watch')
-      ? url.replace('watch?v=', 'embed/')
-      : url;
+    const newUrl = url.includes('watch') ? url.replace('watch?v=', 'embed/') : url;
     return (
       <div>
         <iframe
@@ -69,6 +66,41 @@ function RecipeDetails({ type, id }) {
       </div>
     );
   };
+  const handleShare = () => {
+    const copyText = `http://localhost:3000${history.location.pathname}`;
+    navigator.clipboard.writeText(copyText);
+    alert('Link copied!');
+  };
+
+  const handleFavorite = (typer) => {
+    if (typer === 'drinks') {
+      const localStorage = detail.map((recipe) => (
+        {
+          id: recipe.idDrink,
+          type: 'drink',
+          nationality: '',
+          category: recipe.strCategory,
+          alcoholicOrNot: recipe.strAlcoholic,
+          name: recipe.strDrink,
+          image: recipe.strDrinkThumb,
+        }
+      ));
+      window.localStorage.setItem('favoriteRecipes', JSON.stringify(localStorage));
+    } else if (typer === 'foods') {
+      const localStorage = detail.map((recipe) => (
+        {
+          id: recipe.idMeal,
+          type: 'food',
+          nationality: recipe.strArea,
+          category: recipe.strCategory,
+          alcoholicOrNot: '',
+          name: recipe.strMeal,
+          image: recipe.strMealThumb,
+        }
+      ));
+      window.localStorage.setItem('favoriteRecipes', JSON.stringify(localStorage));
+    }
+  };
 
   if (type === 'foods') {
     return (
@@ -83,29 +115,42 @@ function RecipeDetails({ type, id }) {
               width="420"
               height="345"
             />
-
             <h4>Category</h4>
-            <p data-testid="recipe-category">
-              {item.strCategory}
-            </p>
-
+            <p data-testid="recipe-category">{item.strCategory}</p>
             <h4> Intructions </h4>
-            <p data-testid="instructions">
-              {item.strInstructions}
-            </p>
-
+            <p data-testid="instructions">{item.strInstructions}</p>
             <h4>Ingridients</h4>
-            <ul>
-              {handleIngMeaDrink(Object.entries(item))}
-            </ul>
-
+            <ul>{handleIngMeaDrink(Object.entries(item))}</ul>
             <h4>Recomended Drinks</h4>
             <DetailCards typeOf={ type } />
-
             <h4>YouTube Video</h4>
             {handleYoutube(item.strYoutube)}
+            <Link
+              data-testid="start-recipe-btn"
+              to={ `/${type}/${id}/in-progress` }
+              position="static"
+              style={ { position: 'fixed', bottom: '0px' } }
+            >
+              Continue Recipe
+            </Link>
+            <button
+              data-testid="share-btn"
+              style={ { position: 'fixed', bottom: '0px', marginLeft: '300px' } }
+              type="button"
+              onClick={ handleShare }
+            >
+              Share
+            </button>
+            <button
+              data-testid="favorite-btn"
+              type="button"
+              style={ { position: 'fixed', bottom: '0px', marginLeft: '150px' } }
+              src={ whiteHeartIcon }
+              onClick={ () => handleFavorite(type) }
+            >
+              Favorite
+            </button>
           </div>
-
         ))}
       </div>
     );
@@ -122,41 +167,39 @@ function RecipeDetails({ type, id }) {
               width="420"
               height="345"
             />
-            <h4>Is alcoholic?</h4>
-            <p>
-              {item.strAlcoholic}
-            </p>
+            <h4>Category</h4>
+            <p data-testid="recipe-category">{item.strAlcoholic}</p>
             <h4> Intructions </h4>
-            <p data-testid="instructions">
-              {item.strInstructions}
-            </p>
+            <p data-testid="instructions">{item.strInstructions}</p>
             <h4>Ingridients</h4>
-            <ul>
-              {handleIngMeaDrink(Object.entries(item))}
-            </ul>
+            <ul>{handleIngMeaDrink(Object.entries(item))}</ul>
             <DetailCards typeOf={ type } />
-            {/* {!doneRecipe
-            && (
-              startedRecipe
-                ? (
-                  <Link
-                    data-testid="start-recipe-btn"
-                    to={ `/${type}/${id}/in-progress` }
-                  >
-                    Continue Recipe
-                  </Link>
-                )
-                : (
-                  <Link
-                    data-testid="start-recipe-btn"
-                    to={ `/${type}/${id}/in-progress` }
-                  >
-                    Start Recipe
-                  </Link>
-                )
-            )} */}
+            <Link
+              data-testid="start-recipe-btn"
+              to={ `/${type}/${id}/in-progress` }
+              position="static"
+              style={ { position: 'fixed', bottom: '0px' } }
+            >
+              Continue Recipe
+            </Link>
+            <button
+              data-testid="share-btn"
+              style={ { position: 'fixed', bottom: '0px', marginLeft: '300px' } }
+              type="button"
+              onClick={ handleShare }
+            >
+              Share
+            </button>
+            <button
+              data-testid="favorite-btn"
+              type="button"
+              style={ { position: 'fixed', bottom: '0px', marginLeft: '150px' } }
+              src={ whiteHeartIcon }
+              onClick={ () => handleFavorite(type) }
+            >
+              Favorite
+            </button>
           </div>
-
         ))}
       </div>
     );
