4e646a34c39bcc7c409626b7f5043c935def0c8b
Breno Araujo de Matos <brenoaraujo50@icloud.com>
2022-09-05 11:18:58 -0300

    resolve pagina de done recipes

--- src/services/helpers/renderWithRouter.js
+++ src/services/helpers/renderWithRouter.js
@@ -26,14 +26,29 @@
 
 // src/renderWithRouter.js
 import React from 'react';
-import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import AppProvider from '../../context/AppProvider';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
-  });
-};
-export default renderWithRouter;
+function withRouter(component, history) {
+  return (
+    <Router history={ history }>
+      <AppProvider>
+        { component }
+      </AppProvider>
+    </Router>
+  );
+}
+export default function renderWithRouter(
+  component,
+  {
+    initialPath = '/',
+    history = createMemoryHistory([initialPath]),
+  } = {},
+) {
+  return {
+    ...render(withRouter(component, history)),
+    history,
+  };
+}
